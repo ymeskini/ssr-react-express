453f48afbca27932df241ccc63f07558ae5010f8
Youssef Meskini <youss.meskini@gmail.com>
2019-12-19 08:30:20 +0100

    Simplify the project to be readable

--- src/server/controllers/renderer/renderer.test.ts
+++ src/server/controllers/renderer/renderer.test.ts
@@ -6,17 +6,6 @@
 
 import { Response } from 'express';
 import httpMocks, { MockResponse } from 'node-mocks-http';
-import { get } from '.';
-
-test.skip('should return the get of renderer response', async () => {
-  const req = httpMocks.createRequest({
-    url: '/'
-  });
-  const res = httpMocks.createResponse();
-  const data = (await get(req, res)) as MockResponse<Response>;
-
-  console.log(data._getData());
-});
 
 test('should return 500 when occur internal errors', async () => {
   jest.resetModules();
